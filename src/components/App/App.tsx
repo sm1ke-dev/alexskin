31a7e781901ec179000353c4e14ca0a0740d9c79
sm1ke <smike143@yandex.ru>
2023-10-08 20:46:18 +0500

    feat: настроить роуты

---
+++ src/components/App/App.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Main from "../Main/Main";
+import About from "../About/About";
+import Catalog from "../Catalog/Catalog";
+import Community from "../Community/Community";
+import Service from "../Service/Service";
+import Faq from "../Faq/Faq";
+import Partnership from "../Partnership/Partnership";
+import Documents from "../Documents/Documents";
+
+function App() {
+  return (
+    <div className="page">
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/catalog" element={<Catalog />} />
+        <Route path="/community" element={<Community />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="/faq" element={<Faq />} />
+        <Route path="/partnership" element={<Partnership />} />
+        <Route path="/documents" element={<Documents />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Main.css";
+
+const Main: React.FC = () => {
+  return (
+    <div className="main">
+      <h1 className="main__header">Любить. Ценить. Беречь себя.</h1>
+    </div>
+  );
+};
+
+export default Main;
