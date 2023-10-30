d127583ac72318c42b8e37d0870a0e7ff50d8983
sm1ke <smike143@yandex.ru>
2023-10-30 18:52:42 +0500

    feat: создать страницу Partnership

--- src/components/Partnership/Partnership.tsx
+++ src/components/Partnership/Partnership.tsx
@@ -1,7 +1,47 @@
 import React from "react";
+import Breadcrumbs from "../Breadcrumbs/Breadcrumbs";
+import "./Partnership.css";
 
 const Partnership = () => {
-  return <div>Partnership</div>;
+  return (
+    <main className="partner">
+      <Breadcrumbs />
+      <h2 className="partner__title">Сотрудничество</h2>
+      <ul className="partner__grid">
+        <li className="partner-item">
+          <h3 className="partner-item__title">
+            Приглашаем к сотрудничеству коммерческие организации:
+          </h3>
+          <p className="partner-item__text">
+            - Косметические розничные магазины
+            <br />
+            - Косметологические клиники и салоны красоты
+            <br />
+            - Врачей-косметологов и мастеров-эстетистов
+            <br />- Желающих стать офиц.представителями бренда в вашем городе
+          </p>
+        </li>
+        <li className="partner-item">
+          <h3 className="partner-item__title">
+            По другим вопросам сотрудничества с брендом:
+          </h3>
+          <p className="partner-item__text partner-item__text_wide">
+            Тел: +7 925 712 10 11
+            <br />
+            E-mail: pr.alexskin@gmail.com
+            <br />
+            WhatsApp: https://l.clck.bar/9cf76
+          </p>
+        </li>
+        <li className="partner-item">
+          <h3 className="partner-item__title">
+            По вопросам дистрибуции продукции ALEXSKIN:
+          </h3>
+          <p className="partner-item__text">E-mail: acosmetic23@gmail.com</p>
+        </li>
+      </ul>
+    </main>
+  );
 };
 
 export default Partnership;
